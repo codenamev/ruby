rubygems/rubygems

util/rubocop -A --only Style/TernaryParentheses

test/rubygems/test_gem_stream_ui.rb
@@ -6,7 +6,7 @@ require "timeout"
 class TestGemStreamUI < Gem::TestCase
   # increase timeout with MJIT for --jit-wait testing
   mjit_enabled = defined?(RubyVM::MJIT) && RubyVM::MJIT.enabled?
-  SHORT_TIMEOUT = (RUBY_ENGINE == "ruby" && !mjit_enabled) ? 0.1 : 1.0
+  SHORT_TIMEOUT = RUBY_ENGINE == "ruby" && !mjit_enabled ? 0.1 : 1.0
 
   module IsTty
     attr_accessor :tty

codenamev/ruby

test/rubygems/test_gem_stream_ui.rb
@@ -6,7 +6,7 @@ require "timeout"
 class TestGemStreamUI < Gem::TestCase
   # increase timeout with RJIT for --jit-wait testing
   rjit_enabled = defined?(RubyVM::RJIT) && RubyVM::RJIT.enabled?
-  SHORT_TIMEOUT = (RUBY_ENGINE == "ruby" && !rjit_enabled) ? 0.1 : 1.0
+  SHORT_TIMEOUT = RUBY_ENGINE == "ruby" && !rjit_enabled ? 0.1 : 1.0
 
   module IsTty
     attr_accessor :tty